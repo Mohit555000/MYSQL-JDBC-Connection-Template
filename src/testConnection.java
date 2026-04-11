d7925019b80b7f63a1a3ad81eb43b3cd165333d5
Mohit <gangwanimohit42@gmail.com>
2026-04-11 16:44:58 +0530

    Used old method to connect with the MYSQL Database

--- src/testConnection.java
+++ src/testConnection.java
@@ -2,21 +2,41 @@ package src;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Driver;
 import java.sql.SQLException;
 
 class testConnection{
     public static void main(String[] args) throws SQLException {
+        // ---------------New Method-------------------------
+        // String URL="jdbc:mysql://localhost:3306/testdb";
+        // String user="root";
+        // String password="root";
+
+        // Connection conn=DriverManager.getConnection(URL, user, password);
+        // if(conn==null){
+        //     System.out.println("Connection is not successfull");
+        // }
+        // else{
+        //     System.out.println("Connected Successfully");
+        // }
+        // ----------------Old MEthod-----------------------
+        //Step 1 Creating Driver class object
+        Driver driver=new com.mysql.cj.jdbc.Driver();
+        //Step2 Registering it with the driver class manager
+        DriverManager.registerDriver(driver);
+        //step3 Establis connection
         String URL="jdbc:mysql://localhost:3306/testdb";
         String user="root";
         String password="root";
-
         Connection conn=DriverManager.getConnection(URL, user, password);
         if(conn==null){
-            System.out.println("Connection is not successfull");
+            System.out.println("Connection Unsuccessfull");
         }
         else{
-            System.out.println("Connected Successfully");
+            System.out.println("Connection Unsuccessfull");
+            conn.close();
         }
+
         
     }
 }
