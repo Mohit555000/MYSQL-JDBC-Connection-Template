63b9fe39ead6c020d7c45000ba9f87d974f5a9a6
Mohit555000 <gangwanimohit42@gmail.com>
2026-04-13 12:01:20 +0530

    Update testConnection.java

--- src/testConnection.java
+++ src/testConnection.java
@@ -37,6 +37,12 @@ class testConnection{
             conn.close();
         }
 
+        //Instead of these two lines
+        //Line 1 - Driver driver=new com.mysql.cj.jdbc.Driver();
+        //Line 2 - DriverManager.registerDriver(driver);
+        // we can write a single line as 
+        // Class.forName(com.mysql.cj.jdbc.Driver);
+        // In the backend of Class.forName both the lines (Line 1 and Line 2) are present in the static block so as when we call the static method (forName) (Line 1 and Line 2) automatically gets executed.
         
     }
 }
